3513a13a8b00e201e1ce63a20ee0d3cbb7308916
Dmitry Volkov <dvolkov@moccimedia.com>
2020-10-13 12:55:58 +0300

    calc() game refactored

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -17,23 +17,25 @@ def generate_game():
         tuple: question guessed to the user and correct answer
     """
 
-    # Set list of allowed operators & select random operator for the game
-    opearators_list = ['*', '-', '+']
-    selected_operator = choice(opearators_list)
-
     # Set values used in the calc game
     value_1, value_2 = randint(MIN_RAND_VALUE, MAX_RAND_VALUE), \
         randint(MIN_RAND_VALUE, MAX_RAND_VALUE)
 
+    # Set calculator available operations (alternative to switch-case)
+    operations = {
+        '*': operator.mul(value_1, value_2),
+        '-': operator.sub(value_1, value_2),
+        '+': operator.add(value_1, value_2),
+    }
+
+    # Select random operator from the list
+    selected_operator = choice(list(operations.keys()))
+
     # Ask user for a question
     question = str(value_1) + ' ' + selected_operator + ' ' + str(value_2)
 
-    # Calculate correct answer (alternative to switch-case) by using dict{}
-    correct_answer = {
-        '*': operator.mul(value_1, value_2),
-        '-': operator.sub(value_1, value_2),
-        '+': operator.add(value_1, value_2)
-    }[selected_operator]
+    # Calculate correct answer
+    correct_answer = operations[selected_operator]
 
     # Return function values
     return question, str(correct_answer)
